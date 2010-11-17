2515d3b9c76fbf0c0964aa869556ecf6f4754bd5
Guillermo Paz <guillermo.paz@mercadolibre.com>
2010-11-17 16:19:40 -0300

    SDK fix modal content.data

--- src/js/modal.js
+++ src/js/modal.js
@@ -14,30 +14,30 @@ ui.Modal = function(conf){
 	conf.align = 'center';
 	conf.classes = 'box';
 	
+	conf.ajaxType = 'POST';
+	conf.ajaxParams = [];
+	conf.ajaxParams.push({'x':'x'});//TODO refactor con el header de ajax
+			
+	
 	// Methods Privates
-	var show = function(event){		
-		if(conf.content.type.toLowerCase() === 'ajax') setAjaxConfig();
+	var show = function(event){			
+		conf.content.data = conf.$trigger.attr('href') || conf.$trigger.parents('form').attr('action'); //Se pisaba esta variable porque tiene el mismo class pero content.data diferente. Ejmplo del Type param con contenido distintos pero la misma class (va a traer la del primero)
+		if(conf.content.type.toLowerCase() === 'ajax' && conf.$trigger.attr('type') == 'submit') setAjaxConfig();
 		dimmer.on();
 		that.show(event, conf);
-		$('.close').bind('click', dimmer.off);
+		$('.close').bind('click', dimmer.off);		
 	};
 	
-	var hide = function(event){ 
+	var hide = function(event){
 		dimmer.off();
 		that.hide(event, conf);
 	};
 	
 	var setAjaxConfig = function(){
-		// Content from href/action
-		if(!conf.content.data) conf.content.data = conf.$trigger.attr('href') || conf.$trigger.parents('form').attr('action');
+		// Content from href/action						
 		if(conf.content.data == '') alert('UI: Modal ajax configuration error.'); //TODO mejorar la expresion de vacio
-		
-		// Form method and serialize
-		if(conf.$trigger.attr('type') == 'submit'){
-			conf.ajaxType = conf.$trigger.parents('form').attr('method') || 'POST';
-			conf.ajaxParams = conf.$trigger.parents('form').serializeArray() || [];
-			conf.ajaxParams.push({'x':'x'});//TODO refactor con el header de ajax 
-		};
+		conf.ajaxType = conf.$trigger.parents('form').attr('method');
+		conf.ajaxParams.push(conf.$trigger.parents('form').serializeArray());
 	};
 	
 	// Dimmer
