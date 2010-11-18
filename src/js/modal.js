4dea936904515f04489988925eda0484cdb4e4d0
Guillermo Paz <guillermo.paz@mercadolibre.com>
2010-11-18 09:36:46 -0300

    SDK fix serialized modal data

--- src/js/modal.js
+++ src/js/modal.js
@@ -15,34 +15,32 @@ ui.Modal = function(conf){
 	conf.classes = 'box';
 	
 	conf.ajaxType = 'POST';
-	//conf.ajaxParams = [];
-	//conf.ajaxParams.push({'x':'x'});//TODO refactor con el header de ajax
-	conf.ajaxParams = 'x=x';//TODO refactor con el header de ajax
+	
 			
 	
 	// Methods Privates
-	var show = function(event){			
+	var show = function(event){
 		conf.content.data = conf.$trigger.attr('href') || conf.$trigger.parents('form').attr('action'); //Se pisaba esta variable porque tiene el mismo class pero content.data diferente. Ejmplo del Type param con contenido distintos pero la misma class (va a traer la del primero)
+		conf.ajaxParams = 'x=x';//TODO refactor con el header de ajax
 		if(conf.content.type.toLowerCase() === 'ajax' && conf.$trigger.attr('type') == 'submit') setAjaxConfig();
 		dimmer.on();
 		that.show(event, conf);
 		$('.close').bind('click', hide);
 	};
-	
+
 	var hide = function(event){
 		dimmer.off();
 		that.hide(event, conf);
 	};
-	
+
 	var setAjaxConfig = function(){
 		// Content from href/action						
 		if(conf.content.data == '') alert('UI: Modal ajax configuration error.'); //TODO mejorar la expresion de vacio
-		conf.ajaxType = conf.$trigger.parents('form').attr('method') || 'POST';		
-		//conf.ajaxParams = $.merge(conf.ajaxParams, conf.$trigger.parents('form').serializeArray());
-		var serialized = conf.$trigger.parents('form').serialize();		
+		conf.ajaxType = conf.$trigger.parents('form').attr('method') || 'POST';
+		var serialized = conf.$trigger.parents('form').serialize();
 		conf.ajaxParams = conf.ajaxParams + ((serialized != '') ? '&' + serialized : '');
 	};
-		
+
 	// Dimmer
 	var dimmer = {
 		on:function(){
