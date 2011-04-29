82ef45b522725dd4214da47aad3b27a652779f15
llinares <lean8086@gmail.com>
2011-04-29 17:19:32 -0300

    GH-20 Added keyboard support to Dropdown

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -29,7 +29,6 @@ ch.dropdown = function(conf){
  *  Private Members
  */
 	
-	
 
 /**
  *  Protected Members
@@ -52,16 +51,52 @@ ch.dropdown = function(conf){
         // Show menu
 		that.$content.css('z-index', ch.utils.zIndex ++);
 		that.$trigger.css('z-index', ch.utils.zIndex ++); // Z-index of trigger over content		
-		that.parent.show(event);		
+		that.parent.show(event);
 		that.position("refresh");
 		
 		// Secondary behavior
 		if(conf.skin == "secondary"){
 			that.$container.addClass("ch-dropdown-on"); // Container ON
 		};
-	
-		// Document events
-		ch.utils.document.one('click', function(event){ that.hide(event) });
+		
+		// Close events
+		ch.utils.document.one("click", function(event){ that.hide(event); });
+		ch.utils.document.bind(ch.events.KEY.ESC, function(event){ that.hide(event) });
+		
+		// Select first anchor child by default
+		var items = that.$content.find("a");
+			items.eq(0).focus();
+		
+		// More than one item
+		if(items.length == 0) return that;
+		
+		// Keyboard support
+		var itemSelected = 0;
+		
+		// Item selected by mouseover
+		// TODO: It's over keyboard selection and it is generating double selection.
+		items.each(function(i, e){
+			$(e).bind("mouseenter", function(){
+				itemSelected = i;
+				items.eq( itemSelected ).focus();
+			});
+		});
+		
+		var selectItem = function(arrow, event){
+			that.prevent(event);
+			
+			if(itemSelected == ((arrow == "down") ? items.length - 1 : 0)) return;
+			
+			items.eq( itemSelected ).blur();
+			
+			if(arrow == "down") itemSelected ++; else itemSelected --;
+			
+			items.eq( itemSelected ).focus();
+		};
+		
+		// Arrows
+		ch.utils.document.bind(ch.events.KEY.UP_ARROW, function(x, event){ selectItem("up", event); });
+		ch.utils.document.bind(ch.events.KEY.DOWN_ARROW, function(x, event){ selectItem("down", event); });
 		
         return that;
     };
@@ -77,6 +112,11 @@ ch.dropdown = function(conf){
 		};
 		
         that.parent.hide(event);
+        
+        // Unbind events
+        ch.utils.document.unbind(ch.events.KEY.UP_ARROW);
+        ch.utils.document.unbind(ch.events.KEY.DOWN_ARROW);
+        ch.utils.document.unbind(ch.events.KEY.ESC);
         
         return that;
 	};
