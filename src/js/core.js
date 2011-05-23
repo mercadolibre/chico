e92219e878290fd14a8f63eb6349ab193797bd26
pazguille <guille87paz@gmail.com>
2011-05-23 16:39:53 -0300

    Updated version on core.js

--- src/js/core.js
+++ src/js/core.js
@@ -14,7 +14,7 @@ var ch = window.ch = {
      * @type {Number}
      * @memberOf ch
      */
-    version: "0.6.3",
+    version: "0.6.4",
     /**
      * List of UI components available.
      * @name components
