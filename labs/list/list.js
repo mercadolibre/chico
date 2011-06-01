6ae9bd808c8e6bf05bbbb626421b8c3630323bcf
Natan Santolo <natan.santolo@mercadolibre.com>
2011-06-01 13:02:36 -0300

    GH-95 Improved private method _find

--- labs/list/list.js
+++ labs/list/list.js
@@ -8,6 +8,9 @@
 
 ch.List = ch.Collection = function() {
 
+
+    var that = this;
+
     /**
      * @public
      * @name children
@@ -23,41 +26,36 @@ ch.List = ch.Collection = function() {
      * @param {Number} [q]
      * @param {String} [q]
      * @param {Object} [q]
+     * @param {Function} [a]
      * @return {Object} Returns the finded element
      * @memberOf ch.List
      */
-    var _find = function(q) {
+    var _find = function(q, a) {
         // null search return the entire collection
-        if ( !q && q!=0 ) {
+        if ( !q ) {
             return _children;
         }
-        
+
+        var c = typeof q;
         // number? return a specific position
-        if ( typeof q === "number" ) {
-            return _children[q];
+        if ( c === "number" ) {
+            q--; // _children is a Zero-index based collection
+            return (a) ? a.call( that , q ) : _children[q] ;
         }
         
         // string? ok, let's find it
         var t = size(), _prop, child;
-        if ( typeof q === "string" ) {
+        if ( c === "string" || c === "object" ) {
             while ( t-- ) {
                 child = _children[t];
+                if ( c === "object" && child === q ) {
+                    return (a) ? a.call( that , t ) : child ;
+                }
                 for ( _prop in child ) {
                     if ( _prop === q || child[_prop] === q ) {
-                        return child;
+                        return (a) ? a.call( that , t ) : child ;
                     }
                 } // end for
-            } // end while
-        }
-        
-        // object? ok, assume a item
-        t = size();
-        if ( typeof q === "object") {
-            while ( t-- ) {
-                child = _children[t];
-                if ( child === q ) {
-                    return child;
-                }
             } // end while
         }
     };
@@ -87,38 +85,17 @@ ch.List = ch.Collection = function() {
      */
     var rem = function(q) {
         // null search return
-        if ( !q && q!=0 ) {
+        if ( !q ) {
             return;
         }
         
-        // number? return a specific position
-        if ( typeof q === "number" ) {
-            return _children.splice( q , 1 );
-        }
-        
-        // string? ok, let's find it
-        var t = size(), _prop, child;     
-        if ( typeof q === "string" ) {
-            while ( t-- ) {
-                child = _children[t];
-                for ( _prop in child ) {
-                    if ( _prop === q || child[_prop] === q ) {
-                        return _children.splice( t , 1 );
-                    }
-                } // end for
-            } // end while
+        var remove = function( t ) {
+            console.log(this);
+            console.log(arguments);
+            return _children.splice( t , 1 )[0];
         }
 
-        // object? ok, assume a item
-        t = size();
-        if ( typeof q === "object") {
-            while ( t-- ) {
-                child = _children[t];
-                if ( child === q ) {
-                    return _children.splice( t , 1 );
-                }
-            } // end while
-        }
+        return _find( q , remove );
 
     };
 
