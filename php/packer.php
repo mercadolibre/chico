2f03ab470ff510279778c3e213cb17bea5254057
Natan Santolo <natan.santolo@gmail.com>
2011-07-05 15:43:43 -0300

    GH-233 Added Blink UI Component to Packer.

--- php/packer.php
+++ php/packer.php
@@ -22,7 +22,7 @@ class Packer {
     
     // files to load
     private $files;
-    private $components = "carousel,calendar,dropdown,layer,modal,tabNavigator,tooltip,string,number,custom,required,helper,form,viewer,expando,menu,zoom";
+    private $components = "blink,carousel,calendar,dropdown,layer,modal,tabNavigator,tooltip,string,number,custom,required,helper,form,viewer,expando,menu,zoom";
     private $internals = "cache,positioner,object,floats,navs,controllers,watcher,keyboard,preload,list,extend,onImagesLoads";
 
 
