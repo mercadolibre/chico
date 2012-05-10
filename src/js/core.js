b4a7f0ba586bb9b6de2bc4f246461e2361efb05a
Leandro Linares <lean8086@gmail.com>
2012-05-10 16:20:50 -0300

    Update vendor prefix getter with support to all browsers.

--- src/js/core.js
+++ src/js/core.js
@@ -117,13 +117,21 @@ var ch = window.ch = {
 		// Based on: http://lea.verou.me/2009/02/find-the-vendor-prefix-of-the-current-browser/
 		"VENDOR_PREFIX": (function () {
 			
-			var regex = new RegExp("^(webkit|Moz|ms|O)(?=[A-Z])");
+			var regex = /^(Webkit|Khtml|Moz|ms|O)(?=[A-Z])/,
+			
+				styleDeclaration = document.getElementsByTagName("script")[0].style;
 
-			for (var prop in document.getElementsByTagName("script")[0].style) {
+			for (var prop in styleDeclaration) {
 				if (regex.test(prop)) {
 					return prop.match(regex)[0].toLowerCase();
 				}
 			}
+			
+			// Nothing found so far? Webkit does not enumerate over the CSS properties of the style object.
+			// However (prop in style) returns the correct value, so we'll have to test for
+			// the precence of a specific property
+			if ("WebkitOpacity" in styleDeclaration) { return "webkit"; }
+			if ("KhtmlOpacity" in styleDeclaration) { return "khtml"; }
 
 			return "";
 		}())
