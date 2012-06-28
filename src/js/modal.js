81679683c9cfc5d4e1dda9e9be060395f5042e72
Hernan Mammana <hernanmammana@gmail.com>
2012-06-28 15:20:34 -0300

    GH-670 ch-box class name instead ch-box-lite

--- src/js/modal.js
+++ src/js/modal.js
@@ -48,7 +48,7 @@ ch.modal = function (conf) {
 	var that = this;
 	conf = ch.clon(conf);
 
-	conf.classes = conf.classes || "ch-box-lite";
+	conf.classes = conf.classes || "ch-box";
 	conf.reposition = false;
 
 	// Closable configuration
