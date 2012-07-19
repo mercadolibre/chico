776065d94d2a36a61e66c12bdbf3075ea889b555
pazguille <guille87paz@gmail.com>
2012-07-19 15:43:33 -0300

    #710 CSS Library: support for ch-close

--- src/js/floats.js
+++ src/js/floats.js
@@ -87,7 +87,7 @@ ch.floats = function () {
 				// Append close buttons	
 				// It will close with close button
 				that.$container
-					.prepend("<a class=\"ch-close\" style=\"z-index:" + (ch.utils.zIndex += 1) + "\">×</a>")
+					.prepend("<a class=\"ch-close\" role=\"button\" style=\"z-index:" + (ch.utils.zIndex += 1) + "\"></a>")
 					.bind("click", function (event) {
 						if ($(event.target || event.srcElement).hasClass("ch-close")) { 
 							that.innerHide(event);
