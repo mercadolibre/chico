6bf35ebd0fed214160c229d051eeb786522f4636
pazguille <guille87paz@gmail.com>
2012-08-09 16:12:43 -0300

    Run the project without Apache

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Starting...");
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+	app = module.exports = express();
+
+/**
+ * App configuration.
+ */
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.favicon());
+app.use(app.router);
+app.use('/assets', express.static(__dirname + '/src/shared/assets'));
+app.use('/vendor', express.static(__dirname + '/vendor'));
+
+/**
+ * Render HTML files
+ */
+app.set('views', __dirname + '/views');
+app.set('view options', {'layout': false});
+app.engine('html', require('ejs').renderFile);
+
+/**
+ * Routes
+ */
+require('./routes');
+
+/**
+ * Listen
+ */
+app.listen(3040);
+console.log('Express app started on port 3040');
