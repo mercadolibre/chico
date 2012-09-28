f5c489a8d342fab14e5fa2242fe3bc952b2a745f
pazguille <guille87paz@gmail.com>
2012-09-28 17:09:41 -0300

    Migrate Navs and Expando

--- src/ui/js/ch.Expando.js
+++ src/ui/js/ch.Expando.js
@@ -24,130 +24,151 @@
 * });
 */
 
-ch.expando = function (conf) {
+(function (window, $, ch) {
+	'use strict';
 
-	/**
-	* Reference to a internal component instance, saves all the information and configuration properties.
-	* @private
-	* @name ch.Expando#that
-	* @type object
-	*/
-	var that = this;
-
-	conf = ch.clon(conf);
-	that.conf = conf;
-
-/**
-*	Inheritance
-*/
-
-	that = ch.navs.call(that);
-	that.parent = ch.clon(that);
-
-/**
-*  Protected Members
-*/
-	var $nav = that.$element.children(),
-		triggerAttr = {
-			"aria-expanded":conf.open,
-			"aria-controls":"ch-expando-" + that.uid
-		},
-		contentAttr = {
-			id:triggerAttr["aria-controls"],
-			"aria-hidden":!triggerAttr["aria-expanded"]
-		};
-
-	/**
-	* The component's trigger.
-	* @protected
-	* @name ch.Expando#$trigger
-	* @type jQuery
-	*/
-	that.$trigger = that.$trigger.attr(triggerAttr);
-
-	/**
-	* The component's trigger.
-	* @protected
-	* @name ch.Expando#$content
-	* @type jQuery
-	*/
-	that.$content = $nav.eq(1).attr(contentAttr);
-
-	/**
-	* Shows component's content.
-	* @protected
-	* @function
-	* @name ch.Expando#innerShow
-	* @returns itself
-	*/
-	that.innerShow = function(event){
-		that.$trigger.attr("aria-expanded","true");
-		that.$content.attr("aria-hidden","false");
-		that.parent.innerShow();
-		return that;
+	if (window.ch === undefined) {
+		throw new window.Error('Expected ch namespace defined.');
 	}
 
-	/**
-	* Hides component's content.
-	* @protected
-	* @function
-	* @name ch.Expando#innerHide
-	* @returns itself
-	*/
-	that.innerHide = function(event){
-		that.$trigger.attr("aria-expanded","false");
-		that.$content.attr("aria-hidden","true");
-		that.parent.innerHide();
+	var document = window.document,
+		body = document.body;
+
+	function Expando($el, conf) {
+
+		/**
+		 * Reference to a internal component instance, saves all the information and configuration properties.
+		 * @private
+		 * @name ch.Expando#that
+		 * @type object
+		 */
+		var that = this;
+
+		that.$element = $el;
+		that.element = $el[0];
+		that.type = 'expando';
+		conf = conf || {};
+
+		conf = ch.util.clone(conf);
+		that.conf = conf;
+
+		/**
+		 *	Inheritance
+		 */
+
+		that = ch.Navs.call(that);
+		that.parent = ch.util.clone(that);
+
+		/**
+		 *  Protected Members
+		 */
+		var $nav = that.$element.children(),
+			triggerAttr = {
+				"aria-expanded":conf.open,
+				"aria-controls":"ch-expando-" + that.uid
+			},
+			contentAttr = {
+				id:triggerAttr["aria-controls"],
+				"aria-hidden":!triggerAttr["aria-expanded"]
+			};
+
+		/**
+		 * The component's trigger.
+		 * @protected
+		 * @name ch.Expando#$trigger
+		 * @type jQuery
+		 */
+		that.$trigger = that.$trigger.attr(triggerAttr);
+
+		/**
+		 * The component's trigger.
+		 * @protected
+		 * @name ch.Expando#$content
+		 * @type jQuery
+		 */
+		that.$content = $nav.eq(1).attr(contentAttr);
+
+		/**
+		 * Shows component's content.
+		 * @protected
+		 * @function
+		 * @name ch.Expando#innerShow
+		 * @returns itself
+		 */
+		that.innerShow = function(event){
+			that.$trigger.attr("aria-expanded","true");
+			that.$content.attr("aria-hidden","false");
+			that.parent.innerShow();
+			return that;
+		}
+
+		/**
+		 * Hides component's content.
+		 * @protected
+		 * @function
+		 * @name ch.Expando#innerHide
+		 * @returns itself
+		 */
+		that.innerHide = function(event){
+			that.$trigger.attr("aria-expanded","false");
+			that.$content.attr("aria-hidden","true");
+			that.parent.innerHide();
+			return that;
+		}
+
+
+		/**
+		 *  Public Members
+		 */
+
+		/**
+		 * @borrows ch.Object#uid as ch.Expando#uid
+		 */
+
+		/**
+		 * @borrows ch.Object#element as ch.Expando#element
+		 */
+
+		/**
+		 * @borrows ch.Object#type as ch.Expando#type
+		 */
+
+		/**
+		 * @borrows ch.Navs#show as ch.Expando#show
+		 */
+
+		/**
+		 * @borrows ch.Navs#hide as ch.Expando#hide
+		 */
+
+		/**
+		 *  Default event delegation
+		 */
+
+		that.$trigger.children().attr("role","presentation");
+		ch.util.avoidTextSelection(that.$trigger);
+
+		/**
+		 * Triggers when the component is ready to use (Since 0.8.0).
+		 * @name ch.Expando#ready
+		 * @event
+		 * @public
+		 * @since 0.8.0
+		 * @exampleDescription Following the first example, using <code>widget</code> as expando's instance controller:
+		 * @example
+		 * widget.on("ready",function () {
+		 *	this.show();
+		 * });
+		 */
+		setTimeout(function(){ that.trigger("ready") }, 50);
+
 		return that;
+
 	}
 
+	Expando.prototype.name = 'expando';
+	Expando.prototype.constructor = Expando;
 
-/**
-*  Public Members
-*/
+	ch.factory(Expando);
 
-	/**
-	* @borrows ch.Object#uid as ch.Expando#uid
-	*/
-
-	/**
-	* @borrows ch.Object#element as ch.Expando#element
-	*/
-
-	/**
-	* @borrows ch.Object#type as ch.Expando#type
-	*/
-
-	/**
-	* @borrows ch.Navs#show as ch.Expando#show
-	*/
-
-	/**
-	* @borrows ch.Navs#hide as ch.Expando#hide
-	*/
-
-/**
-*  Default event delegation
-*/
-
-	that.$trigger.children().attr("role","presentation");
-	ch.utils.avoidTextSelection(that.$trigger);
-
-	/**
-	* Triggers when the component is ready to use (Since 0.8.0).
-	* @name ch.Expando#ready
-	* @event
-	* @public
-	* @since 0.8.0
-	* @exampleDescription Following the first example, using <code>widget</code> as expando's instance controller:
-	* @example
-	* widget.on("ready",function () {
-	*	this.show();
-	* });
-	*/
-	setTimeout(function(){ that.trigger("ready") }, 50);
-
-	return that;
-};
-
-ch.factory("expando");
+}(this, this.jQuery, this.ch));
--- src/ui/js/ch.Navs.js
+++ src/ui/js/ch.Navs.js
@@ -12,225 +12,245 @@
 * @see ch.Dropdown
 * @see ch.Expando
 */
+(function (window, $, ch) {
+	'use strict';
 
-ch.navs = function () {
+	if (window.ch === undefined) {
+		throw new window.Error('Expected ch namespace defined.');
+	}
 
-	/**
-	* Reference to a internal component instance, saves all the information and configuration properties.
-	* @private
-	* @name ch.Navs#that
-	* @type object
-	*/
-	var that = this,
-		conf = that.conf;
+	var document = window.document,
+		$html = $('html'),
+		body = document.body;
 
-	conf.icon = ch.utils.hasOwn(conf, "icon") ? conf.icon : true;
-	conf.open = conf.open || false;
-	conf.fx = conf.fx || false;
+	function Navs($el, conf) {
+		/**
+		 * Reference to a internal component instance, saves all the information and configuration properties.
+		 * @private
+		 * @name ch.Navs#that
+		 * @type object
+		 */
+		var that = this,
+			conf = that.conf;
 
-/**
-*	Inheritance
-*/
-
-	that = ch.uiobject.call(that);
-	that.parent = ch.clon(that);
-
-/**
-*	Protected Members
-*/
-	/**
-	* Status of component
-	* @protected
-	* @name ch.Navs#active
-	* @returns boolean
-	*/
-	that.active = false;
-
-	/**
-	* The component's trigger.
-	* @private
-	* @name ch.Navs#$trigger
-	* @type jQuery
-	*/
-	that.$trigger = that.$element.children().eq(0);
-
-	/**
-	* The component's content.
-	* @private
-	* @name ch.Navs#$content
-	* @type jQuery
-	*/
-	that.$content = that.$element.children().eq(1);
-
-	/**
-	* Shows component's content.
-	* @protected
-	* @name ch.Navs#innerShow
-	* @returns itself
-	*/
-	that.innerShow = function (event) {
-		that.prevent(event);
-
-		if (that.active) {
-			return that.innerHide(event);
-		}
-
-		that.active = true;
-
-		that.$trigger.addClass("ch-" + that["type"] + "-trigger-on");
+		conf.icon = ch.util.hasOwn(conf, "icon") ? conf.icon : true;
+		conf.open = conf.open || false;
+		conf.fx = conf.fx || false;
 
 		/**
-		* onShow callback function
-		* @name ch.Navs#onShow
-		* @event
-		*/
-		// Animation
-		if (conf.fx) {
-			that.$content.slideDown("fast", function () {
+		 *	Inheritance
+		 */
+
+		that = ch.Uiobject.call(that);
+		that.parent = ch.util.clone(that);
+
+		/**
+		 *	Protected Members
+		 */
+
+		/**
+		 * Status of component
+		 * @protected
+		 * @name ch.Navs#active
+		 * @returns boolean
+		 */
+		that.active = false;
+
+		/**
+		 * The component's trigger.
+		 * @private
+		 * @name ch.Navs#$trigger
+		 * @type jQuery
+		 */
+		that.$trigger = that.$element.children().eq(0);
+
+		/**
+		 * The component's content.
+		 * @private
+		 * @name ch.Navs#$content
+		 * @type jQuery
+		 */
+		that.$content = that.$element.children().eq(1);
+
+		/**
+		 * Shows component's content.
+		 * @protected
+		 * @name ch.Navs#innerShow
+		 * @returns itself
+		 */
+		that.innerShow = function (event) {
+			that.prevent(event);
+
+			if (that.active) {
+				return that.innerHide(event);
+			}
+
+			that.active = true;
+
+			that.$trigger.addClass("ch-" + that["type"] + "-trigger-on");
+
+			/**
+			 * onShow callback function
+			 * @name ch.Navs#onShow
+			 * @event
+			 */
+			// Animation
+			if (conf.fx) {
+				that.$content.slideDown("fast", function () {
+					// new callbacks
+					that.trigger("show");
+					// old callback system
+					that.callbacks("onShow");
+				});
+			} else {
 				// new callbacks
 				that.trigger("show");
 				// old callback system
 				that.callbacks("onShow");
-			});
-		} else {
-			// new callbacks
-			that.trigger("show");
-			// old callback system
-			that.callbacks("onShow");
-		}
+			}
 
-		that.$content.removeClass("ch-hide");
+			that.$content.removeClass("ch-hide");
 
-		return that;
-	};
+			return that;
+		};
 
-	/**
-	* Hides component's content.
-	* @protected
-	* @function
-	* @name ch.Navs#innerHide
-	* @returns itself
-	*/
-	that.innerHide = function (event) {
-		that.prevent(event);
-
-		if (!that.active) { return; }
-
-		that.active = false;
-
-		that.$trigger.removeClass("ch-" + that["type"] + "-trigger-on");
 		/**
-		* onHide callback function
-		* @name ch.Navs#onHide
-		* @event
-		*/
-		// Animation
-		if (conf.fx) {
-			that.$content.slideUp("fast", function () {
-				that.callbacks("onHide");
-			});
-		} else {
-			// new callbacks
-			that.trigger("hide");
-			// old callback system
-			that.callbacks("onHide");
-		}
+		 * Hides component's content.
+		 * @protected
+		 * @function
+		 * @name ch.Navs#innerHide
+		 * @returns itself
+		 */
+		that.innerHide = function (event) {
+			that.prevent(event);
 
-		that.$content.addClass("ch-hide");
+			if (!that.active) { return; }
+
+			that.active = false;
+
+			that.$trigger.removeClass("ch-" + that["type"] + "-trigger-on");
+
+			/**
+			 * onHide callback function
+			 * @name ch.Navs#onHide
+			 * @event
+			 */
+			// Animation
+			if (conf.fx) {
+				that.$content.slideUp("fast", function () {
+					that.callbacks("onHide");
+				});
+			} else {
+				// new callbacks
+				that.trigger("hide");
+				// old callback system
+				that.callbacks("onHide");
+			}
+
+			that.$content.addClass("ch-hide");
+
+			return that;
+		};
+
+		/**
+		 * Create component's layout
+		 * @protected
+		 * @function
+		 * @name ch.Navs#configBehavior
+		 */
+		that.configBehavior = function () {
+			that.$trigger
+				.addClass("ch-" + that.type + "-trigger")
+				.bind("click", function (event) { that.innerShow(event); });
+
+			that.$content.addClass("ch-" + that.type + "-content ch-hide");
+
+			// Visual configuration
+			if ($html.hasClass("lt-ie8") && conf.icon) {
+				$("<span class=\"ch-" + that.type + "-ico\">Drop</span>").appendTo(that.$trigger);
+			}
+
+			if (conf.open) { that.innerShow(); }
+
+		};
+
+		/**
+		 * Public Members
+		 */
+
+		/**
+		 * Shows component's content.
+		 * @public
+		 * @function
+		 * @name ch.Navs#show
+		 * @returns itself
+		 */
+		that["public"].show = function(){
+			that.innerShow();
+			return that["public"];
+		};
+
+		/**
+		 * Hides component's content.
+		 * @public
+		 * @function
+		 * @name ch.Navs#hide
+		 * @returns itself
+		 */
+		that["public"].hide = function(){
+			that.innerHide();
+			return that["public"];
+		};
+
+		/**
+		 * Returns a Boolean if the component's core behavior is active. That means it will return 'true' if the component is on and it will return false otherwise.
+		 * @public
+		 * @function
+		 * @name ch.Navs#isActive
+		 * @returns boolean
+		 */
+		that["public"].isActive = function () {
+			return that.active;
+		};
+
+		/**
+		 *	Default event delegation
+		 */
+
+		that.configBehavior();
+		that.$element.addClass("ch-" + that.type);
+
+		/**
+		 * Triggers when component is visible.
+		 * @name ch.Navs#show
+		 * @event
+		 * @public
+		 * @example
+		 * widget.on("show",function () {
+		 *	otherComponent.hide();
+		 * });
+		 * @see ch.Navs#event:show
+		 */
+
+		/**
+		 * Triggers when component is not longer visible.
+		 * @name ch.Navs#hide
+		 * @event
+		 * @public
+		 * @example
+		 * widget.on("hide",function () {
+		 *	otherComponent.show();
+		 * });
+		 * @see ch.Navs#event:hide
+		 */
 
 		return that;
-	};
 
-	/**
-	* Create component's layout
-	* @protected
-	* @function
-	* @name ch.Navs#configBehavior
-	*/
-	that.configBehavior = function () {
-		that.$trigger
-			.addClass("ch-" + that.type + "-trigger")
-			.bind("click", function (event) { that.innerShow(event); });
+	}
 
-		that.$content.addClass("ch-" + that.type + "-content ch-hide");
+	Navs.prototype.name = 'navs';
+	Navs.prototype.constructor = Navs;
 
-		// Visual configuration
-		if (ch.utils.html.hasClass("lt-ie8") && conf.icon) {
-			$("<span class=\"ch-" + that.type + "-ico\">Drop</span>").appendTo(that.$trigger);
-		}
+	ch.Navs = Navs;
 
-		if (conf.open) { that.innerShow(); }
-
-	};
-
-/**
-* Public Members
-*/
-	/**
-	* Shows component's content.
-	* @public
-	* @function
-	* @name ch.Navs#show
-	* @returns itself
-	*/
-	that["public"].show = function(){
-		that.innerShow();
-		return that["public"];
-	};
-
-	/**
-	* Hides component's content.
-	* @public
-	* @function
-	* @name ch.Navs#hide
-	* @returns itself
-	*/
-	that["public"].hide = function(){
-		that.innerHide();
-		return that["public"];
-	};
-
-	/**
-	* Returns a Boolean if the component's core behavior is active. That means it will return 'true' if the component is on and it will return false otherwise.
-	* @public
-	* @function
-	* @name ch.Navs#isActive
-	* @returns boolean
-	*/
-	that["public"].isActive = function () {
-		return that.active;
-	};
-
-/**
-*	Default event delegation
-*/
-
-	that.configBehavior();
-	that.$element.addClass("ch-" + that.type);
-
-	/**
-	* Triggers when component is visible.
-	* @name ch.Navs#show
-	* @event
-	* @public
-	* @example
-	* widget.on("show",function () {
-	*	otherComponent.hide();
-	* });
-	* @see ch.Navs#event:show
-	*/
-
-	/**
-	* Triggers when component is not longer visible.
-	* @name ch.Navs#hide
-	* @event
-	* @public
-	* @example
-	* widget.on("hide",function () {
-	*	otherComponent.show();
-	* });
-	* @see ch.Navs#event:hide
-	*/
-
-	return that;
-}
+}(this, this.jQuery, this.ch));
