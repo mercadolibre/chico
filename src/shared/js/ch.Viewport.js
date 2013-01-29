48225250b26d2ef8e50fe68993dfb84f4a18f964
Hernan Mammana <hernanmammana@gmail.com>
2013-01-29 16:01:28 -0300

    GH-924 Unify with mobile

--- src/shared/js/ch.Viewport.js
+++ src/shared/js/ch.Viewport.js
@@ -6,176 +6,177 @@
  * @memberOf ch
  */
 (function (window, $, ch) {
-	'use strict';
+    'use strict';
 
-	if (ch === undefined) {
-		throw new window.Error('Expected ch namespace defined.');
-	}
+    if (ch === undefined) {
+        throw new window.Error('Expected ch namespace defined.');
+    }
 
-	var $window = $(window),
-		resized = false,
-		scrolled = false;
+    var $window = $(window),
+        resized = false,
+        scrolled = false;
 
-	$window
-		.on('resize', function () { resized = true; })
-		.on('scroll', function () { scrolled = true; });
+    $window
+        .on('resize', function () { resized = true; })
+        .on('scroll', function () { scrolled = true; });
 
-	function update() {
-		// No changing, exit
-		if (!resized && !scrolled) { return; }
+    function update() {
+        // No changing, exit
+        if (!resized && !scrolled) { return; }
 
-		var eve = (resized) ? 'resize' : 'scroll';
+        var eve = (resized) ? 'resize' : 'scroll';
 
-		// Refresh viewport
-		this.refresh();
+        // Refresh viewport
+        this.refresh();
 
-		// Change status
-		resized = false;
-		scrolled = false;
+        // Change status
+        resized = false;
+        scrolled = false;
 
-		// Emits the current event
-		this.emit(eve);
-	}
+        // Emits the current event
+        //this.emit(eve);
+    }
 
-	function Viewport() {
-		ch.EventEmitter.call(this);
-		this.init();
-	}
+    function Viewport() {
+        ch.EventEmitter.call(this);
+        this.init();
+    }
 
-	Viewport.prototype.constructor = Viewport;
+    Viewport.prototype.constructor = Viewport;
 
-	Viewport.prototype.init = function () {
-		var that = this;
+    Viewport.prototype.init = function () {
+        var that = this;
 
-		/**
-		 * Element representing the visible area.
-		 * @public
-		 * @name ch.Viewport#element
-		 * @type Object
-		 */
-		that.$el = $window;
+        /**
+         * Element representing the visible area.
+         * @public
+         * @name ch.Viewport#element
+         * @type Object
+         */
+        that.$el = $window;
 
-		that.refresh();
+        that.refresh();
 
-		window.setInterval(function () {
-			update.call(that);
-		}, 350);
-	};
+        window.setInterval(function () {
+            update.call(that);
+        }, 350);
+    };
 
-	/**
-	 * Calculates/updates the dimensions (width and height) of viewport (in pixels).
-	 * @public
-	 * @function
-	 * @name ch.Viewport#calculateDimensions
-	 */
-	Viewport.prototype.calculateDimensions = function () {
-		/**
-		 * Height of the visible area.
-		 * @public
-		 * @name ch.Viewport#height
-		 * @type Number
-		 */
-		this.height = this.$el.height();
+    /**
+     * Calculates/updates the dimensions (width and height) of viewport (in pixels).
+     * @public
+     * @function
+     * @name ch.Viewport#calculateDimensions
+     */
+    Viewport.prototype.calculateDimensions = function () {
+        /**
+         * Height of the visible area.
+         * @public
+         * @name ch.Viewport#height
+         * @type Number
+         */
+        this.height = this.$el.height();
 
-		/**
-		 * Width of the visible area.
-		 * @public
-		 * @name ch.Viewport#width
-		 * @type Number
-		 */
-		this.width = this.$el.width();
-	};
+        /**
+         * Width of the visible area.
+         * @public
+         * @name ch.Viewport#width
+         * @type Number
+         */
+        this.width = this.$el.width();
+    };
 
-	/**
-	 * Calculates/updates the viewport position.
-	 * @public
-	 * @function
-	 * @name ch.Viewport#calculateOffset
-	 */
-	Viewport.prototype.calculateOffset = function () {
+    /**
+     * Calculates/updates the viewport position.
+     * @public
+     * @function
+     * @name ch.Viewport#calculateOffset
+     */
+    Viewport.prototype.calculateOffset = function () {
+        var that = this;
 
-		/**
-		 * Top offset of the visible area.
-		 * @public
-		 * @name ch.Viewport#top
-		 * @type Number
-		 */
-		this.top = this.$el.scrollTop();
+        /**
+         * Top offset of the visible area.
+         * @public
+         * @name ch.Viewport#top
+         * @type Number
+         */
+        this.top = that.$el[0].scrollTop || that.$el[0].scrollY;
 
-		/**
-		 * Left offset of the visible area.
-		 * @public
-		 * @name ch.Viewport#left
-		 * @type Number
-		 */
-		this.left = this.$el.scrollLeft();
+        /**
+         * Left offset of the visible area.
+         * @public
+         * @name ch.Viewport#left
+         * @type Number
+         */
+        this.left = that.$el[0].scrollLeft || that.$el[0].scrollX;
 
-		/**
-		 * Right offset of the visible area.
-		 * @public
-		 * @name ch.Viewport#right
-		 * @type Number
-		 */
-		this.right = this.left + this.width;
+        /**
+         * Right offset of the visible area.
+         * @public
+         * @name ch.Viewport#right
+         * @type Number
+         */
+        this.right = this.left + this.width;
 
-		/**
-		 * Bottom offset of the visible area.
-		 * @public
-		 * @name ch.Viewport#bottom
-		 * @type Number
-		 */
-		this.bottom = this.top + this.height;
-	};
+        /**
+         * Bottom offset of the visible area.
+         * @public
+         * @name ch.Viewport#bottom
+         * @type Number
+         */
+        this.bottom = this.top + this.height;
+    };
 
-	/**
-	 * Rertuns the device orientation: landscape or portrait.
-	 * @public
-	 * @function
-	 * @name ch.Viewport#calculateOrientation
-	 * @returns {String}
-	 */
-	Viewport.prototype.calculateOrientation = function () {
-		this.orientation = (Math.abs(this.$el.orientation) === 90) ? 'landscape' : 'portrait';
-	};
+    /**
+     * Rertuns the device orientation: landscape or portrait.
+     * @public
+     * @function
+     * @name ch.Viewport#calculateOrientation
+     * @returns {String}
+     */
+    Viewport.prototype.calculateOrientation = function () {
+        this.orientation = (Math.abs(this.$el.orientation) === 90) ? 'landscape' : 'portrait';
+    };
 
-	/**
-	 * Calculates if an element is completely located in the viewport.
-	 * @public
-	 * @function
-	 * @name ch.Viewport#inViewport
-	 * @returns {Boolean}
-	 */
-	Viewport.prototype.inViewport = function (el) {
-		var r = el.getBoundingClientRect();
+    /**
+     * Calculates if an element is completely located in the viewport.
+     * @public
+     * @function
+     * @name ch.Viewport#inViewport
+     * @returns {Boolean}
+     */
+    Viewport.prototype.inViewport = function (el) {
+        var r = el.getBoundingClientRect();
 
-		return (r.top > 0) && (r.right < this.width) && (r.bottom < this.height) && (r.left > 0);
-	};
+        return (r.top > 0) && (r.right < this.width) && (r.bottom < this.height) && (r.left > 0);
+    };
 
-	/**
-	 * Calculates if an element is visible in the viewport.
-	 * @public
-	 * @function
-	 * @name ch.Viewport#isVisible
-	 * @returns {Boolean}
-	 */
-	Viewport.prototype.isVisible = function (el) {
-		var r = el.getBoundingClientRect();
+    /**
+     * Calculates if an element is visible in the viewport.
+     * @public
+     * @function
+     * @name ch.Viewport#isVisible
+     * @returns {Boolean}
+     */
+    Viewport.prototype.isVisible = function (el) {
+        var r = el.getBoundingClientRect();
 
-		return (r.height >= this.top);
-	};
+        return (r.height >= this.top);
+    };
 
-	/**
-	 * Upadtes the viewport dimension, viewport positions and orietation.
-	 * @public
-	 * @function
-	 * @name ch.Viewport#refresh
-	 */
-	Viewport.prototype.refresh = function () {
-		this.calculateDimensions();
-		this.calculateOffset();
-		this.calculateOrientation();
-	};
+    /**
+     * Upadtes the viewport dimension, viewport positions and orietation.
+     * @public
+     * @function
+     * @name ch.Viewport#refresh
+     */
+    Viewport.prototype.refresh = function () {
+        this.calculateDimensions();
+        this.calculateOffset();
+        this.calculateOrientation();
+    };
 
-	ch.viewport = new Viewport();
+    ch.viewport = new Viewport();
 
 }(this, (this.jQuery || this.Zepto), this.ch));
