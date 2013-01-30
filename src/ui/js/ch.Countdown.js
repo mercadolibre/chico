583640f7301b7abbc98c08698c50265ecc498bca
Hernan Mammana <hernanmammana@gmail.com>
2013-01-30 12:37:07 -0300

    GH-926 & GH-927 Migrated. Stoped the automatic cut of the text when it exceeds the limit.

--- src/ui/js/ch.Countdown.js
+++ src/ui/js/ch.Countdown.js
@@ -31,7 +31,9 @@
 
 	var setTimeout = window.setTimeout;
 
-	function Countdown($el, conf) {
+
+
+	function Countdown($el, options) {
 
 		/**
 		* Reference to an internal component instance, saves all the information and configuration properties.
@@ -41,133 +43,12 @@
 		*/
 		var that = this;
 
-		that.$element = $el;
-		that.element = $el[0];
-		that.type = 'countdown';
-		conf = conf || {};
-
-		conf = ch.util.clone(conf);
+		that.init($el, options);
 
 		// Configuration by default
 		// Max length of content
-		conf.max = parseInt(conf.max) || conf.num || parseInt(conf.content) || 500;
+		//conf.max = parseInt(conf.max) || conf.num || parseInt(conf.content) || 500;
 
-		// Messages
-		conf.plural = conf.plural || "# characters left.";
-		conf.singular = conf.singular || "# character left.";
-
-		that.conf = conf;
-
-	/**
-	*	Inheritance
-	*/
-
-		that = ch.Controls.call(that);
-		that.parent = ch.util.clone(that);
-
-	/**
-	*	Private Members
-	*/
-		/**
-		* Length of value of form control.
-		* @private
-		* @name ch.Countdown#contentLength
-		* @type Number
-		*/
-		var contentLength = that.element.value.length,
-
-		/**
-		* Amount of free characters until full the field.
-		* @private
-		* @name ch.Countdown#remaining
-		* @type Number
-		*/
-			remaining = conf.max - contentLength,
-
-		/**
-		* Change the visible message of remaining characters.
-		* @private
-		* @name ch.Countdown#updateRemaining
-		* @function
-		* @param num {Number} Remaining characters.
-		*/
-			updateRemaining = (function () {
-
-				// Singular or Plural message depending on amount of remaining characters
-				var message = (remaining === 1) ? conf.singular : conf.plural,
-
-				// Append to container to allow icon aside inputs
-					$container = that.$element.parent(),
-
-				// Create the DOM Element when message will be shown
-					$display = $("<p class=\"ch-form-hint\">" + message.replace("#", remaining) + "</p>").appendTo($container);
-
-				// Real function
-				return function (num) {
-
-					// Singular or Plural message depending on amount of remaining characters
-					var message = (num !== 1 ? conf.plural : conf.singular).replace(/\#/g, num);
-
-					// Update DOM text
-					$display.text(message);
-
-					// Update amount of remaining characters
-					remaining = num;
-
-				};
-
-			}());
-
-	/**
-	*	Protected Members
-	*/
-
-		/**
-		* Process input of data on form control and updates remaining amount of characters or limits the content length
-		* @protected
-		* @name ch.Countdown#process
-		* @function
-		*/
-		that.process = function () {
-
-			var len = that.element.value.length;
-
-			// Countdown or Countup
-			if ((len > contentLength && len <= conf.max) || (len < contentLength && len >= 0)) {
-
-				// Change visible message of remaining characters
-				updateRemaining(remaining - (len - contentLength));
-
-				// Update length of value of form control.
-				contentLength = len;
-
-			// Limit Count
-			} else if (len > contentLength && len > conf.max) {
-
-				// Cut the string value of form control
-				that.element.value = that.element.value.substr(0, conf.max);
-
-			};
-
-		};
-
-
-	/**
-	*	Public Members
-	*/
-
-		/**
-		 * @borrows ch.Widget#uid as ch.Menu#uid
-		 * @borrows ch.Widget#element as ch.Menu#element
-		 * @borrows ch.Widget#type as ch.Menu#type
-		 */
-
-	/**
-	*	Default event delegation
-	*/
-
-		// Bind process function to element
-		that.$element.on("keyup keypress paste", function () { setTimeout(that.process, 0); });
 
 		/**
 		* Triggers when component is ready to use.
@@ -177,13 +58,132 @@
 		* @exampleDescription Following the first example, using <code>widget</code> as Countdown's instance controller:
 		* @example
 		* widget.on("ready",function () {
-		*	this.element;
+		*	this.el;
 		* });
 		*/
-		setTimeout(function () { that.trigger("ready"); }, 50);
+		setTimeout(function () { that.emit("ready"); }, 50);
 
-		return that['public'];
+		return that;
+
+	}
+
+	/**
+	*	Private Members
+	*/
+
+		/**
+		 * Inheritance
+		 */
+
+	var parent = ch.util.inherits(Countdown, ch.Widget);
+
+	Countdown.prototype._defaults = {
+		'plural': '# characters left.',
+		'singular': '# character left.',
+		'max': 500
 	};
+
+	Countdown.prototype.init = function($el, options) {
+		var that = this;
+
+		parent.init.call(this, $el, options);
+
+		if(this._options.num !== undefined && this._options.num !== this._options.max) {
+			this._options.max = this._options.num;
+		}
+
+		/**
+		* Amount of free characters until full the field.
+		* @private
+		* @name ch.Countdown#remaining
+		* @type Number
+		*/
+		this._remaining = this._options.max - this._contentLength();
+
+		// Singular or Plural message depending on amount of remaining characters
+		var message = ((this._remaining === 1) ? this._options.singular : this._options.plural),
+			messageID = 'ch-countdown-message-'+ this.uid,
+
+			// Append to container to allow icon aside inputs
+			$container = that.$el.parent(),
+
+			// Create the DOM Element when message will be shown
+			$display = $('<p class="ch-form-hint" id="'+ messageID +'">' + message.replace('#', this._remaining) + '</p>').appendTo($container);
+
+		this._$message = $('#' + messageID, $container);
+
+		// Bind process function to element
+		this.$el.on('keyup keypress keydown paste', function () { that.update() });
+	}
+
+	/**
+	* Length of value of form control.
+	* @private
+	* @name ch.Countdown#contentLength
+	* @type Number
+	*/
+	Countdown.prototype._contentLength = function() {
+		return this.el.value.length;
+	}
+
+	/**
+	* Process input of data on form control and updates remaining amount of characters or limits the content length. Also, change the visible message of remaining characters.
+	* @protected
+	* @name ch.Countdown#process
+	* @function
+	*/
+	Countdown.prototype.update = function () {
+		var that = this,
+			length = that._contentLength();
+
+		that._remaining = that._options.max - length;
+
+		// Limit Count alert the user
+		if (length <= that._options.max) {
+
+			if(that.$el.hasClass('ch-validation-error')){
+				that.$el.removeClass('ch-validation-error');
+				that._$message.removeClass('ch-countdown-exceeded');
+				that.$el.attr('aria-invalid', 'false');
+			}
+
+		} else if (length > that._options.max) {
+
+			/**
+			 * Triggers when the lenght of characters is exceeded
+			 * @name ch.Calendar#exceeded
+			 * @event
+			 * @public
+			 * @exampleDescription If you want to advice the user when it's exceeding the limit.
+			 * @example
+			 * widget.on("exceeded", function () {
+			 * 	alert('Hey you are exceeding the limit of this field.');
+			 * });
+			 */
+			that.emit('exceeded');
+
+			that.$el.addClass('ch-validation-error');
+			that.$el.attr('aria-invalid', 'true');
+			that._$message.addClass('ch-countdown-exceeded');
+
+		};
+
+		// Change visible message of remaining characters
+		// Singular or Plural message depending on amount of remaining characters
+		var message = (that._remaining !== 1 ? that._options.plural : that._options.singular).replace(/\#/g, that._remaining);
+
+		// Update DOM text
+		that._$message.text(message);
+
+		return that;
+
+	};
+
+	/**
+	 * @borrows ch.Widget#uid as ch.Countdown#uid
+	 * @borrows ch.Widget#element as ch.Countdown#element
+	 * @borrows ch.Widget#type as ch.Countdown#type
+	 */
 
 	Countdown.prototype.name = 'countdown';
 	Countdown.prototype.constructor = Countdown;
