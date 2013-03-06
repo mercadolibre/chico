54e2c0d06f8c6b3dbd753cde1b92f28f6b0d3687
pazguille <guille87paz@gmail.com>
2013-03-06 12:16:25 -0300

    #566 Closable: The time of close must be configurable.

--- src/ui/js/ch.Zoom.js
+++ src/ui/js/ch.Zoom.js
@@ -77,6 +77,7 @@
         'height': '300px',
         'open': 'mouseenter',
         'close': 'mouseleave',
+        'closeDelay': 0,
         'side': 'right',
         'align': 'top',
         'offsetX': 20,
