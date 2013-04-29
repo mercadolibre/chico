22a8ff9aa117754c044807332d0a570dafc167cf
Hernan Mammana <hernanmammana@gmail.com>
2013-04-29 10:31:05 -0300

    Working Offline. All images are now stored in the project.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.favicon());
 app.use(app.router);
 app.use('/assets', express.static(__dirname + '/src/shared/assets'));
 app.use('/vendor', express.static(__dirname + '/vendor'));
+app.use('/assets/demo', express.static(__dirname + '/views/assets'));
 app.use('/test', express.static(__dirname + '/tests'));
 app.use('/libs', express.static(__dirname + '/libs'));
 
