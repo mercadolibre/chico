eca43e36b23b7f055d015cef298971f652493b76
pazguille <guille87paz@gmail.com>
2013-05-03 17:09:35 -0300

    #987 Menu: Update tests. Add test to .show(), .hide() and .getShow() methods.

--- tests/menu/spect.js
+++ tests/menu/spect.js
@@ -1,187 +1,176 @@
+var menu1 = $("#menu-1").menu({'fx': false}),
+    readyEvent = jasmine.createSpy('readyEvent'),
+    showEvent = jasmine.createSpy('showEvent'),
+    hideEvent = jasmine.createSpy('hideEvent'),
+    $el = menu1.$el,
+    $children = menu1.$el.children(),
+    $expandable = menu1._children[0],
+    $bellows = menu1.$el.children(':last-child');
+
 describe('Menu', function () {
-    var menu1 = $("#menu-1").menu(),
-        menu2 = $("#menu-2").menu({'icon': false}),
-        menu3 = $("#menu-3").menu({'selected': '1#1'}),
+    menu1.on('ready', function () { readyEvent(); })
 
-        $el = $(menu1.element),
-        $children = $el.children(),
-        $bellows = $el.children(':last-child'),
-        $trigger = $el.children().children(':first-child').eq(0),
-        $content = $el.children().children(':last-child').eq(0),
-
-        selectCallback = jasmine.createSpy('selectCallback'),
-        selectEvent = jasmine.createSpy('selectEvent'),
-        readyEvent = jasmine.createSpy('readyEvent'),
-
-        menu4 = $("#menu-4").menu({
-            'onSelect': function () { selectCallback(); }
-        });
-
-    menu4
-        .on('ready', function () { readyEvent(); })
-        .on('select', function () { selectEvent(); });
-
-    it('Should be defined', function () {
-        expect(ch.util.hasOwn(ch, 'Menu')).toBeTruthy();
+    it('should be defined on ch object', function () {
+        expect(ch.hasOwnProperty('Menu')).toBeTruthy();
         expect(typeof ch.Menu).toEqual('function');
     });
 
-    describe('Shold have the following public properties:', function () {
-
-        it('.element', function () {
-            expect(ch.util.hasOwn(menu1, 'element')).toBeTruthy();
-            expect(menu1.element.nodeType).toEqual(1);
-        });
-
-        it('.type / .name', function () {
-            expect(ch.util.hasOwn(menu1, 'type')).toBeTruthy();
-            expect(typeof menu1.type).toEqual('string');
-            expect(menu1.type).toEqual('menu');
-        });
-
-        it('.constructor', function () {
-            expect(ch.util.hasOwn(menu1, 'constructor')).toBeTruthy();
-            expect(typeof menu1.constructor).toEqual('function');
-        });
-
-        it('.uid', function () {
-            expect(typeof menu1.uid).toEqual('number');
-        });
-
+    it('should be defined on $ object', function () {
+        expect($.fn.hasOwnProperty('menu')).toBeTruthy();
+        expect(typeof $.fn.menu).toEqual('function');
     });
 
-    describe('Shold have the following public methods:', function () {
+    it('should be return a new instance', function () {
+        expect(menu1 instanceof ch.Menu).toBeTruthy();
+    });
 
-        it('.select()', function () {
-            expect(ch.util.hasOwn(menu1, 'select')).toBeTruthy();
-            expect(typeof menu1.select).toEqual('function');
-        });
-
-        it('.off()', function () {
-            expect(ch.util.hasOwn(menu1, 'off')).toBeTruthy();
-            expect(typeof menu1.off).toEqual('function');
-        });
-
-        it('.on()', function () {
-            expect(ch.util.hasOwn(menu1, 'on')).toBeTruthy();
-            expect(typeof menu1.on).toEqual('function');
-        });
-
-        it('.once()', function () {
-            expect(ch.util.hasOwn(menu1, 'once')).toBeTruthy();
-            expect(typeof menu1.once).toEqual('function');
-        });
-
-        it('.trigger()', function () {
-            expect(ch.util.hasOwn(menu1, 'trigger')).toBeTruthy();
-            expect(typeof menu1.trigger).toEqual('function');
+    it('should emit the "ready" event when it\'s created', function () {
+        waits(50);
+        runs(function () {
+            expect(readyEvent).toHaveBeenCalled();
         });
     });
 
-    describe('Shold have the following ID and Classnames:', function () {
-        it('.ch-menu', function () {
-            expect($el.hasClass('ch-menu')).toBeTruthy();
-        });
+    it('should use the EventEmitter ability', function () {
+        expect(menu1.on).not.toEqual(undefined);
+        expect(typeof menu1.on).toEqual('function');
+    });
+});
 
-        it('.ch-expandable', function () {
-            expect($children.hasClass('ch-expandable')).toBeTruthy();
-        });
+describe('It should have the following public properties:', function () {
 
-        it('.ch-bellows', function () {
-            expect($bellows.hasClass('ch-bellows')).toBeTruthy();
-        });
+    it('.el', function () {
+        expect(menu1.el).not.toEqual(undefined);
+        expect(menu1.el.nodeType).toEqual(1);
     });
 
-    describe('Shold have the following ARIA attributes:', function () {
-        it('role="navigation"', function () {
-            expect($el.attr('role')).toEqual('navigation');
-        });
-
-        it('role="presentation"', function () {
-            expect($bellows.attr('role')).toEqual('presentation');
-        });
+    it('.$el', function () {
+        expect(menu1.$el).not.toEqual(undefined);
+        expect(menu1.$el instanceof $).toBeTruthy();
     });
 
-    describe('By defult', function () {
-        it('Shold have a icon', function () {
-            expect($trigger.hasClass('ch-expandable-ico')).toBeTruthy();
-        });
-
-        it('Shold be closed', function () {
-            expect($content.hasClass('ch-hide')).toBeTruthy();
-        });
+    it('.name', function () {
+        expect(menu1.name).not.toEqual(undefined);
+        expect(typeof menu1.name).toEqual('string');
+        expect(menu1.name).toEqual('menu');
     });
 
-    describe('Public methods', function () {
-        it('.select()', function () {
-            // Getter
-            var selected = menu1.select();
-            expect(selected).toEqual('');
-
-            // Setter
-            menu1.select(1);
-            selected = menu1.select();
-            expect(selected).toEqual(1);
-
-            expect($trigger.hasClass('ch-expandable-trigger-on')).toBeTruthy();
-            expect($content.hasClass('ch-hide')).toBeFalsy();
-
-            menu1.select(2);
-            expect($children.eq(1).children(':first-child').hasClass('ch-expandable-trigger-on')).toBeTruthy();
-            expect($children.eq(1).children(':last-child').hasClass('ch-hide')).toBeFalsy();
-        });
+    it('.constructor', function () {
+        expect(menu1.constructor).not.toEqual(undefined);
+        expect(typeof menu1.constructor).toEqual('function');
     });
 
-    describe('A intance configured without icon', function () {
-        it('Shouldn\'t have the icon classname', function () {
-            expect($(menu2.element).children(':first-child').children(':first-child').hasClass('ch-expandable-ico')).toBeFalsy();
-        });
+    it('.uid', function () {
+        expect(menu1.uid).not.toEqual(undefined);
+        expect(typeof menu1.uid).toEqual('number');
     });
 
-    describe('A instance configured open by default', function () {
-        it('Should have the open classname', function () {
-            expect($(menu3.element).children(':first-child').children(':first-child').hasClass('ch-expandable-trigger-on')).toBeTruthy();
-            expect($(menu3.element).children(':first-child').children(':last-child').hasClass('ch-hide')).toBeFalsy();
-        });
-    });
+});
 
-    describe('Should execute the following callbacks:', function () {
-        beforeEach(function () {
-            listener = jasmine.createSpy('listener');
-        });
+describe('It should have the following public methods:', function () {
+    var methods = ['init', 'destroy', 'show', 'hide', 'getShown', 'content', 'enable', 'disable'],
+        i = 0,
+        len = methods.length;
 
-        it('select', function () {
-            menu4.select(1);
-            waits(500)
-            runs(function () {
-                expect(selectCallback).toHaveBeenCalled();
-                menu4.select(1);
+    for (i; i < len; i += 1) {
+        (function (i){
+            it('.' + methods[i] + '()', function () {
+                expect(menu1[methods[i]]).not.toEqual(undefined);
+                expect(typeof menu1[methods[i]]).toEqual('function');
             });
+        }(i));
+    }
+});
 
+describe('It should have a wrapper and', function () {
+
+    it('should exist', function () {
+        expect($el).not.toEqual(undefined);
+        expect($el[0].nodeType).toEqual(1);
+        expect($el instanceof $).toBeTruthy();
+    });
+
+    it('should have the ".ch-menu" class name', function () {
+        expect($el.hasClass('ch-menu')).toBeTruthy();
+    });
+
+    it('should have the WAI-ARIA role "navigation"', function () {
+       expect($el.attr('role')).toEqual('navigation');
+    });
+});
+
+describe('It should have children and it', function () {
+
+    it('have a "ch-menu-fold" class name', function () {
+        expect($children.hasClass('ch-menu-fold')).toBeTruthy();
+    });
+
+    describe('should be Expandables and', function () {
+        it('should exist', function () {
+            expect($expandable instanceof ch.Expandable).toBeTruthy();
+        });
+
+        it('its container should have the WAI-ARIA role "menu"', function () {
+            expect($children.children(':last-child').attr('role')).toEqual('menu');
+        });
+
+        it('the links insid its container should have the WAI-ARIA role "menuitem"', function () {
+            expect($children.children(':last-child').children().children().attr('role')).toEqual('menuitem');
         });
 
     });
 
-    describe('Should execute the following events:', function () {
+    it('should have the WAI-ARIA role "presentation" if it\'s not expandables', function () {
+        expect($bellows.attr('role')).toEqual('presentation');
+    });
 
-        beforeEach(function () {
-            listener = jasmine.createSpy('listener');
-        });
+});
 
-        it('ready', function () {
-            waits(50);
-            runs(function () {
-                expect(readyEvent).toHaveBeenCalled();
-            });
+describe('Its show() method', function () {
+    menu1.on('show', function () { showEvent(); })
 
-        });
+    it('should be call with parameters', function () {
+        menu1.show(1);
+        expect($children.eq(0).children(':last-child').hasClass('ch-hide')).toBeFalsy();
+    });
 
-        it('select', function () {
-            menu4.select(1);
-            waits(500)
-            runs(function () {
-                expect(selectEvent).toHaveBeenCalled();
-            });
-        });
+    it('should emit the "show" event', function () {
+        expect(showEvent).toHaveBeenCalled();
+    });
+});
+
+describe('Its hide() method', function () {
+    menu1.on('hide', function () { hideEvent(); })
+
+    it('should be call with parameters', function () {
+        menu1.hide(1);
+        expect($children.eq(0).children(':last-child').hasClass('ch-hide')).toBeTruthy();
+    });
+
+    it('should emit the "hide" event', function () {
+        expect(hideEvent).toHaveBeenCalled();
+    });
+});
+
+describe('Its getShown() method', function () {
+    it('should return a number of current shown child', function () {
+        menu1.show(1);
+        expect(menu1.getShown()).toEqual(jasmine.any(Number));
+    });
+
+    it('should return a collection of shown children', function () {
+        menu1.show(1);
+        expect(ch.util.isArray(menu1.getShown())).toBeTruthy();
+    });
+
+    it('should return a undefined when its children are hidden', function () {
+        menu1.hide(1).hide(2);
+        expect(menu1.getShown()).toBeUndefined();
+    });
+});
+
+describe('An instance by default', function () {
+    it('should has its containers hidden', function () {
+        expect($children.eq(0).children(':last-child').hasClass('ch-hide')).toBeTruthy();
     });
 });
