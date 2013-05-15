db7e5e6989338e65819ee75bff5b159f496ddc62
pazguille <guille87paz@gmail.com>
2013-05-15 17:45:56 -0300

    #987 Email: Update and create new tests.

--- tests/email/spect.js
+++ tests/email/spect.js
@@ -1,53 +1,31 @@
+var email = $('#input_user').email('Please, enter a valid email.');
+
 describe('ch.Email', function () {
-	var form = '<form id="form{ID}" action="./" class="ch-form"><div class="ch-form-row"><label>Test {ID}</label><input id="validation{ID}" type="text"></div><div class="ch-form-actions"><input type="submit" class="ch-btn"></div></form>',
-		idGenerator = (function(){
-			var count = 0;
+    it('should be a function', function () {
+        expect(typeof ch.Email).toEqual('function');
+    });
 
-			return function(){
-				return count++;
-			}
-		}());
+    it('should be defined on ch object', function () {
+        expect(ch.hasOwnProperty('Email')).toBeTruthy();
+        expect(typeof ch.Email).toEqual('function');
+    });
 
-	describe('ch.Email global initialization and returned object.', function () {
-		var n = idGenerator();
-		var f = $(form.replace(/{ID}/g, n));
-		var input = f.find('#validation' + n);
-		var validation = input.email();
-		$('body').prepend(f);
+    it('should be defined on $ object', function () {
+        expect($.fn.hasOwnProperty('email')).toBeTruthy();
+        expect(typeof $.fn.email).toEqual('function');
+    });
 
-		it('ch.Email should be a function.', function () {
-			expect(typeof ch.Email).toEqual('function');
-		});
+    it('should be return a new instance', function () {
+        expect(email instanceof ch.Validation).toBeTruthy();
+    });
 
-		it('ch.Email should return an object.', function () {
-			expect(typeof validation).toEqual('object');
-		});
+    it('should return an error when the value is diferent to a valid email', function () {
+        email.$el.val('Test');
+        expect(email.hasError()).toBeTruthy();
+    });
 
-	});
-
-	describe('ch.Email working and configuration.', function () {
-		var n = idGenerator();
-		var message = 'This is a new test!.';
-		var f = $(form.replace(/{ID}/g, n));
-		var input = f.find('#validation' + n);
-		var validation = input.email(message);
-		$('body').prepend(f);
-
-		it('ch.Email should return an error when numbers are set.', function () {
-			input.attr('value', 1234);
-			expect(validation.hasError()).toBeTruthy();
-		});
-
-		it('ch.Email should return the same text send as a parameter when it was initalized.', function () {
-			expect(validation.validator.conditions.email.message).toEqual(message);
-		});
-
-		it('ch.Email should not return an error when string are set.', function () {
-			input.attr('value', 'foo@foo.bar');
-			expect(validation.hasError()).toBeFalsy();
-		});
-
-	});
-
-
+    it('should remove the error when the value is a valid email', function () {
+        email.$el.val('chico@mercadolibre.com');
+        expect(email.hasError()).toBeFalsy();
+    });
 });
