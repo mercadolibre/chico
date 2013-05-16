8685a8d7512b7fc64f6cd8e8c98baa76e4bfb312
pazguille <guille87paz@gmail.com>
2013-05-16 12:43:26 -0300

    #987 maxLength: Update and create new tests.

--- tests/maxLength/spect.js
+++ tests/maxLength/spect.js
@@ -1,53 +1,32 @@
+var maxLength = $('#input_user').maxLength(30);
+
 describe('ch.MaxLength', function () {
-	var form = '<form id="form{ID}" action="./" class="ch-form"><div class="ch-form-row"><label>Test {ID}</label><input id="validation{ID}" type="text"></div><div class="ch-form-actions"><input type="submit" class="ch-btn"></div></form>',
-		idGenerator = (function(){
-			var count = 0;
+    it('should be a function', function () {
+        expect(typeof ch.MaxLength).toEqual('function');
+    });
 
-			return function(){
-				return count++;
-			}
-		}());
+    it('should be defined on ch object', function () {
+        expect(ch.hasOwnProperty('MaxLength')).toBeTruthy();
+        expect(typeof ch.MaxLength).toEqual('function');
+    });
 
-	describe('ch.MaxLength global initialization and returned object.', function () {
-		var n = idGenerator();
-		var f = $(form.replace(/{ID}/g, n));
-		var input = f.find('#validation' + n);
-		var validation = input.maxLength(5);
-		$('body').prepend(f);
+    it('should be defined on $ object', function () {
+        expect($.fn.hasOwnProperty('maxLength')).toBeTruthy();
+        expect(typeof $.fn.maxLength).toEqual('function');
+    });
 
-		it('ch.MaxLength should be a function.', function () {
-			expect(typeof ch.MaxLength).toEqual('function');
-		});
+    it('should be return a new instance', function () {
+        expect(maxLength instanceof ch.Validation).toBeTruthy();
+    });
 
-		it('ch.MaxLength should return an object.', function () {
-			expect(typeof validation).toEqual('object');
-		});
+    it('should return an error when the value is higher than "maxLength" number', function () {
+        maxLength.$el.val('The string length is higher than maxLength number.');
+        expect(maxLength.hasError()).toBeTruthy();
+    });
 
-	});
-
-	describe('ch.MaxLength working and configuration.', function () {
-		var n = idGenerator();
-		var message = 'This is a new test!.';
-		var f = $(form.replace(/{ID}/g, n));
-		var input = f.find('#validation' + n);
-		var validation = input.maxLength(4,message);
-		$('body').prepend(f);
-
-		it('ch.MaxLength should return an error when numbers are set.', function () {
-			input.attr('value', 'abcde');
-			expect(validation.hasError()).toBeTruthy();
-		});
-
-		it('ch.MaxLength should return the same text send as a parameter when it was initalized.', function () {
-			expect(validation.validator.conditions.maxLength.message).toEqual(message);
-		});
-
-		it('ch.MaxLength should not return an error when string are set.', function () {
-			input.attr('value', 'abc');
-			expect(validation.hasError()).toBeFalsy();
-		});
-
-	});
-
+    it('should return an error when the value is smaller than "maxLength" number', function () {
+        maxLength.$el.val('The string length is smaller');
+        expect(maxLength.hasError()).toBeFalsy();
+    });
 
 });
