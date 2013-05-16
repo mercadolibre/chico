682527f3b44cfd161f46c2f6f129f50968b39d2d
pazguille <guille87paz@gmail.com>
2013-05-16 13:06:22 -0300

    #987 minLength: Update and create new tests.

--- tests/minLength/spect.js
+++ tests/minLength/spect.js
@@ -1,53 +1,32 @@
+var minLength = $('#input_user').minLength(30);
+
 describe('ch.MinLength', function () {
-	var form = '<form id="form{ID}" action="./" class="ch-form"><div class="ch-form-row"><label>Test {ID}</label><input id="validation{ID}" type="text"></div><div class="ch-form-actions"><input type="submit" class="ch-btn"></div></form>',
-		idGenerator = (function(){
-			var count = 0;
+    it('should be a function', function () {
+        expect(typeof ch.MinLength).toEqual('function');
+    });
 
-			return function(){
-				return count++;
-			}
-		}());
+    it('should be defined on ch object', function () {
+        expect(ch.hasOwnProperty('MinLength')).toBeTruthy();
+        expect(typeof ch.MinLength).toEqual('function');
+    });
 
-	describe('ch.MinLength global initialization and returned object.', function () {
-		var n = idGenerator();
-		var f = $(form.replace(/{ID}/g, n));
-		var input = f.find('#validation' + n);
-		var validation = input.minLength(5);
-		$('body').prepend(f);
+    it('should be defined on $ object', function () {
+        expect($.fn.hasOwnProperty('minLength')).toBeTruthy();
+        expect(typeof $.fn.minLength).toEqual('function');
+    });
 
-		it('ch.MinLength should be a function.', function () {
-			expect(typeof ch.MinLength).toEqual('function');
-		});
+    it('should be return a new instance', function () {
+        expect(minLength instanceof ch.Validation).toBeTruthy();
+    });
 
-		it('ch.MinLength should return an object.', function () {
-			expect(typeof validation).toEqual('object');
-		});
+    it('should return an error when the value is higher than "minLength" number', function () {
+        minLength.$el.val('The string length is higher than minLength number.');
+        expect(minLength.hasError()).toBeFalsy();
+    });
 
-	});
-
-	describe('ch.MinLength working and configuration.', function () {
-		var n = idGenerator();
-		var message = 'This is a new test!.';
-		var f = $(form.replace(/{ID}/g, n));
-		var input = f.find('#validation' + n);
-		var validation = input.minLength(4,message);
-		$('body').prepend(f);
-
-		it('ch.MinLength should return an error when numbers are set.', function () {
-			input.attr('value', 'abc');
-			expect(validation.hasError()).toBeTruthy();
-		});
-
-		it('ch.MinLength should return the same text send as a parameter when it was initalized.', function () {
-			expect(validation.validator.conditions.minLength.message).toEqual(message);
-		});
-
-		it('ch.MinLength should not return an error when string are set.', function () {
-			input.attr('value', 'abcde');
-			expect(validation.hasError()).toBeFalsy();
-		});
-
-	});
-
+    it('should return an error when the value is smaller than "minLength" number', function () {
+        minLength.$el.val('The string length is smaller');
+        expect(minLength.hasError()).toBeTruthy();
+    });
 
 });
