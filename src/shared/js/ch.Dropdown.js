10295f2045aed258943e383f04742013796a5b55
llinares <lean8086@gmail.com>
2013-08-01 12:29:54 -0300

    GH-989 Dropdown: Using the ARIA active descendant directly on elements.

--- src/shared/js/ch.Dropdown.js
+++ src/shared/js/ch.Dropdown.js
@@ -93,11 +93,9 @@
 
         parent.init.call(this, $el, options);
 
-        this.activeDescendant = 'ch-dropdown' + this.uid + '-selected';
-
         this.$trigger
             .addClass('ch-dropdown-trigger')
-            .prop('aria-activedescendant', this.activeDescendant);
+            .prop('aria-activedescendant', 'ch-dropdown' + this.uid + '-selected');
 
         ch.util.avoidTextSelection(this.$trigger);
 
@@ -171,7 +169,7 @@
 
         // Selects new current option
         this._$navigation[this._selected].focus();
-        this._$navigation[this._selected].id = this.activeDescendant;
+        this._$navigation[this._selected].id = 'ch-dropdown' + this.uid + '-selected';
 
         return this;
     }
