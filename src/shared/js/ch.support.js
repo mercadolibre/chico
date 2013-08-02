53279d4f8dc96e33e1e44067c051693ba3056f41
Guille Paz <guille87paz@gmail.com>
2013-08-02 15:51:23 -0300

    Update support documentation.

--- src/shared/js/ch.support.js
+++ src/shared/js/ch.support.js
@@ -8,18 +8,30 @@
          * Verify that CSS Transitions are supported (or any of its browser-specific implementations).
          * @type {Boolean}
          * @link http://gist.github.com/373874
+         * @example
+         * if (ch.support.transition) {
+         *     // Some code here!
+         * }
          */
         'transition': body.style.WebkitTransition !== undefined || body.style.MozTransition !== undefined || body.style.MSTransition !== undefined || body.style.OTransition !== undefined || body.style.transition !== undefined,
 
         /**
-         * Description.
+         * Checks if the $ library has fx methods.
          * @type {Boolean}
+         * @example
+         * if (ch.support.fx) {
+         *     // Some code here!
+         * }
          */
         'fx': !!$.fn.slideDown,
 
         /**
-         * Description.
+         * Checks if the User Agent support touch events.
          * @type {Boolean}
+         * @example
+         * if (ch.support.touch) {
+         *     // Some code here!
+         * }
          */
         'touch': 'createTouch' in document
     };
