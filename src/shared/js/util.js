bcfb5848bc1fd18abe11153aacf453d7461c724a
Hernan Mammana <hernanmammana@gmail.com>
2014-08-18 12:08:18 -0300

    #1210 util.js, jQuery methods isPlainObject & extend are now in ch.util

--- src/shared/js/util.js
+++ src/shared/js/util.js
@@ -445,23 +445,103 @@
                 }
             }
         }()),
-        'extend': function(obj) {
-            var source,
-                prop,
-                i,
-                length = arguments.length;
 
-            if (!(typeof obj === 'function' || typeof obj === 'object' && !!obj)) { return obj; }
+        /**
+         * Extends an object with other object
+         * @name extend
+         * @param {Object} target The destination of the other objects
+         * @param {Object} obj1 The objects to be merged
+         * @param {Object} objn The objects to be merged
+         * @see Based on: <a href="https://github.com/jquery" target="_blank">https://github.com/jquery/</a>
+         * @example
+         * ch.util.extend(target, obj1, objn);
+         */
+        'extend': function() {
+            var options,
+                name,
+                src,
+                copy,
+                copyIsArray,
+                clone,
+                target = arguments[0] || {},
+                i = 1,
+                length = arguments.length,
+                deep = false;
 
-            for (i = 1; i < length; i++) {
-                source = arguments[i];
-                for (prop in source) {
-                    if (Object.prototype.hasOwnProperty.call(source, prop)) {
-                        obj[prop] = source[prop];
+            // Handle a deep copy situation
+            if (typeof target === "boolean") {
+                deep = target;
+
+                // Skip the boolean and the target
+                target = arguments[i] || {};
+                i++;
+            }
+
+            // Handle case when target is a string or something (possible in deep copy)
+            if (typeof target !== "object" && !typeof target === 'function') {
+                target = {};
+            }
+
+            // Extend jQuery itself if only one argument is passed
+            if (i === length) {
+                target = this;
+                i--;
+            }
+
+            for (; i < length; i++) {
+                // Only deal with non-null/undefined values
+                if ((options = arguments[i]) != null) {
+                    // Extend the base object
+                    for ( name in options ) {
+                        src = target[name];
+                        copy = options[name];
+
+                        // Prevent never-ending loop
+                        if ( target === copy ) {
+                            continue;
+                        }
+
+                        // Recurse if we're merging plain objects or arrays
+                        if (deep && copy && (ch.util.isPlainObject(copy) || (copyIsArray = ch.util.isArray(copy)) ) ) {
+
+                            if (copyIsArray) {
+                                copyIsArray = false;
+                                clone = src && ch.util.isArray(src) ? src : [];
+
+                            } else {
+                                clone = src && ch.util.isPlainObject(src) ? src : {};
+                            }
+
+                            // Never move original objects, clone them
+                            target[name] = ch.util.extend( deep, clone, copy );
+
+                        // Don't bring in undefined values
+                        } else if (copy !== undefined) {
+                            target[name] = copy;
+                        }
                     }
                 }
             }
 
-            return obj;
+            // Return the modified object
+            return target;
+        },
+
+        'isPlainObject': function(obj) {
+            // Not plain objects:
+            // - Any object or value whose internal [[Class]] property is not "[object Object]"
+            // - DOM nodes
+            // - window
+            if (typeof obj !== "object" || obj.nodeType || (obj != null && obj === obj.window)) {
+                return false;
+            }
+
+            if (obj.constructor && !Object.prototype.hasOwnProperty.call(obj.constructor.prototype, "isPrototypeOf")) {
+                return false;
+            }
+
+            // If the function hasn't returned already, we're confident that
+            // |obj| is a plain object, created by {} or constructed with new Object
+            return true;
         }
     };
