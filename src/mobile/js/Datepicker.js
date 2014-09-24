778990f7dc9868aa607fcb09bcdd4eccdd9ac035
Hernan Mammana <hernanmammana@gmail.com>
2014-09-24 09:16:06 -0300

    #1210 Datepicker.js, mobile version added working without $

---
+++ src/mobile/js/Datepicker.js
@@ -0,0 +1,12 @@
+(function (Datepicker) {
+    'use strict';
+
+    /**
+     * Configuration by default.
+     * @type {Object}
+     * @private
+     */
+    Datepicker.prototype._defaults.align = 'right';
+
+
+}(this.ch.Datepicker));
