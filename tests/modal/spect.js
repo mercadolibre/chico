2bd316c9caf182d1c249bd076224f7a6d9bba5e6
Hernan Mammana <hernanmammana@gmail.com>
2014-12-09 10:10:12 -0300

    #1210 Modal tests done

--- tests/modal/spect.js
+++ tests/modal/spect.js
@@ -1,20 +1,15 @@
-var modal = $('#modal').modal({
+var modal = new ch.Modal(document.getElementById('modal'), {
         'content': 'test',
         'fx': 'none'
     }).show(),
-    $t = modal.$trigger,
-    $c = modal.$container;
+    t = modal.trigger,
+    c = modal.container;
 
 describe('Modal', function () {
 
     it('should be defined on ch object', function () {
         expect(ch.hasOwnProperty('Modal')).toBeTruthy();
         expect(typeof ch.Modal).toEqual('function');
-    });
-
-    it('should be defined on $ object', function () {
-        expect($.fn.hasOwnProperty('modal')).toBeTruthy();
-        expect(typeof $.fn.modal).toEqual('function');
     });
 
     it('should return a new instance of Modal', function () {
@@ -25,36 +20,36 @@ describe('Modal', function () {
 describe('It should have a container with', function () {
 
     it('role dialog', function () {
-        expect($c.attr('role')).toEqual('dialog');
+        expect(c.getAttribute('role')).toEqual('dialog');
     });
 
     it('specific CSS class names', function () {
-        expect($c.hasClass('ch-modal')).toBeTruthy();
-        expect($c.hasClass('ch-box-lite')).toBeTruthy();
+        expect(ch.util.classList(c).contains('ch-modal')).toBeTruthy();
+        expect(ch.util.classList(c).contains('ch-box-lite')).toBeTruthy();
     });
 
     it('specific width', function () {
-        expect($c[0].style.width).toEqual('50%');
+        expect(c.style.width).toEqual('50%');
     });
 
     it('alignment', function () {
-        expect($c.attr('data-side')).toEqual('center');
-        expect($c.attr('data-align')).toEqual('center');
+        expect(c.getAttribute('data-side')).toEqual('center');
+        expect(c.getAttribute('data-align')).toEqual('center');
     });
 });
 
 describe('It should have an underlay', function () {
 
     it('that exists', function () {
-        expect($('.ch-underlay').length).toEqual(1);
+        expect(document.querySelectorAll('.ch-underlay').length).toEqual(1);
     });
 
     it('with tabindex -1', function () {
-        expect($('.ch-underlay')[0].tabIndex).toEqual(-1);
+        expect(document.querySelector('.ch-underlay').getAttribute('tabindex')).toEqual('-1');
     });
 
     it('that dissapears when Modal closes', function () {
         modal.hide();
-        expect($('.ch-underlay').length).toEqual(0);
+        expect(document.querySelectorAll('.ch-underlay').length).toEqual(0);
     });
 });
