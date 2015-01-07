b49979fb786b175696f22ca0373002c1fbd5235e
Hernan Mammana <hernanmammana@gmail.com>
2015-01-07 17:37:39 -0300

    #1210 Tooltip test done

--- tests/tooltip/spect.js
+++ tests/tooltip/spect.js
@@ -1,20 +1,15 @@
-var tooltip = $('#tooltip').tooltip({
+var tooltip = new ch.Tooltip(document.getElementById('tooltip'), {
         'content': 'test',
         'fx': 'none'
     }).show(),
-    $t = tooltip.$trigger,
-    $c = tooltip.$container;
+    t = tooltip.trigger,
+    c = tooltip.container;
 
 describe('Tooltip', function () {
 
     it('should be defined on ch object', function () {
         expect(ch.hasOwnProperty('Tooltip')).toBeTruthy();
         expect(typeof ch.Tooltip).toEqual('function');
-    });
-
-    it('should be defined on $ object', function () {
-        expect($.fn.hasOwnProperty('tooltip')).toBeTruthy();
-        expect(typeof $.fn.tooltip).toEqual('function');
     });
 
     it('should return a new instance of Layer', function () {
@@ -25,11 +20,11 @@ describe('Tooltip', function () {
 describe('It should have a container with', function () {
 
     it('role tooltip', function () {
-        expect($c.attr('role')).toEqual('tooltip');
+        expect(c.getAttribute('role')).toEqual('tooltip');
     });
 
     it('specific CSS class names', function () {
-        expect($c.hasClass('ch-tooltip')).toBeTruthy();
-        expect($c.hasClass('ch-cone')).toBeTruthy();
+        expect(ch.util.classList(c).contains('ch-tooltip')).toBeTruthy();
+        expect(ch.util.classList(c).contains('ch-cone')).toBeTruthy();
     });
 });
