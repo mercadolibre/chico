5de8801c27eaf57df856055d0e5b2649b4dd065d
Oleh Burkhay <oleh.burkhay@mercadolibre.cl>
2015-06-11 09:19:58 -0300

    Pointertap moved to PointerEvents polyfill

--- src/shared/scripts/events.js
+++ src/shared/scripts/events.js
@@ -1,5 +1,7 @@
 /**
- * Every time Chico UI needs to inform all visual components that layout has been changed, it emits this event.
+ * Every time Chico UI needs to inform all visual components that layout has
+ * been changed, it emits this event.
+ *
  * @constant
  * @memberof ch
  * @type {String}
@@ -23,61 +25,70 @@ ch.onresize = 'resize';
 ch.onscroll = 'scroll';
 
 /**
- * Equivalent to 'touchstart' or 'mousedown', depending on device capabilities.
+ * Equivalent to 'pointerdown' or 'mousedown', depending on browser capabilities.
+ *
  * @constant
  * @memberof ch
  * @type {String}
- * @link http://www.w3.org/TR/2013/WD-pointerevents-20130115/#dfn-pointerdown | Pointer Events W3C Working Draft
+ * @link http://www.w3.org/TR/pointerevents/#dfn-pointerdown | Pointer Events W3C Recommendation
  */
 ch.onpointerdown = window.MouseEvent ? 'pointerdown' : 'mousedown';
 
 /**
- * Equivalent to 'touchend' or 'mouseup', depending on device capabilities.
+ * Equivalent to 'pointerup' or 'mouseup', depending on browser capabilities.
+ *
  * @constant
  * @memberof ch
  * @type {String}
- * @link http://www.w3.org/TR/2013/WD-pointerevents-20130115/#dfn-pointerup | Pointer Events W3C Working Draft
+ * @link http://www.w3.org/TR/pointerevents/#dfn-pointerup | Pointer Events W3C Recommendation
  */
 ch.onpointerup = window.MouseEvent ? 'pointerup' : 'mouseup';
 
 /**
- * Equivalent to 'touchmove' or 'mousemove', depending on device capabilities.
+ * Equivalent to 'pointermove' or 'mousemove', depending on browser capabilities.
+ *
  * @constant
  * @memberof ch
  * @type {String}
- * @link http://www.w3.org/TR/2013/WD-pointerevents-20130115/#dfn-pointermove | Pointer Events W3C Working Draft
+ * @link http://www.w3.org/TR/pointerevents/#dfn-pointermove | Pointer Events W3C Recommendation
  */
 ch.onpointermove = window.MouseEvent ? 'pointermove' : 'mousemove';
 
 /**
- * Equivalent to 'touchend' or 'click', depending on device capabilities.
+ * Equivalent to 'pointertap' or 'click', depending on browser capabilities.
+ *
  * @constant
  * @memberof ch
  * @type {String}
- * @link http://www.w3.org/TR/2013/WD-pointerevents-20130115/#list-of-pointer-events | Pointer Events W3C Working Draft
+ * @link http://www.w3.org/TR/pointerevents/#list-of-pointer-events | Pointer Events W3C Recommendation
  */
 ch.onpointertap = (ch.support.touch && window.MouseEvent) ? 'pointertap' : 'click';
 
 /**
- * Equivalent to 'touchstart' or 'mouseenter', depending on device capabilities.
+ * Equivalent to 'pointerenter' or 'mouseenter', depending on browser capabilities.
+ *
  * @constant
  * @memberof ch
  * @type {String}
- * @link http://www.w3.org/TR/2013/WD-pointerevents-20130115/#dfn-pointerenter | Pointer Events W3C Working Draft
+ * @link http://www.w3.org/TR/pointerevents/#dfn-pointerenter | Pointer Events W3C Recommendation
  */
 ch.onpointerenter = window.MouseEvent ? 'pointerenter' : 'mouseenter';
 
 /**
- * Equivalent to 'touchend' or 'mouseleave', depending on device capabilities.
+ * Equivalent to 'pointerleave' or 'mouseleave', depending on browser capabilities.
+ *
  * @constant
  * @memberof ch
  * @type {String}
- * @link http://www.w3.org/TR/2013/WD-pointerevents-20130115/#dfn-pointerleave | Pointer Events W3C Working Draft
+ * @link http://www.w3.org/TR/pointerevents/#dfn-pointerleave | Pointer Events W3C Recommendation
  */
 ch.onpointerleave = window.MouseEvent ? 'pointerleave' : 'mouseleave';
 
 /**
- * Alphanumeric keys event.
+ * The DOM input event that is fired when the value of an <input> or <textarea>
+ * element is changed. Equivalent to 'input' or 'keydown', depending on browser
+ * capabilities.
+ *
  * @constant
  * @memberof ch
  * @type {String}
@@ -86,11 +97,10 @@ ch.onkeyinput = ('oninput' in document.createElement('input')) ? 'input' : 'keyd
 
 /**
  * Event utility
+ *
  * @constant
- * @memberof ch.util
+ * @memberof ch
  * @type {Object}
- * @example
- * ch.Event.addListener(document, 'click', function(){}, false);
  */
 ch.Event = (function () {
     var isStandard = document.addEventListener ? true : false,
@@ -104,9 +114,34 @@ ch.Event = (function () {
     }
 
     return {
+        /**
+         * Crossbrowser implementation of {HTMLElement}.addEventListener.
+         *
+         * @memberof ch.Event
+         * @type {Function}
+         * @param {HTMLElement} el An HTMLElement to add listener to
+         * @param {String} evt Event name
+         * @param {Function} fn Event handler function
+         * @param {Boolean} bubbles Whether or not to be propagated to outer elements.
+         * @example
+         * ch.Event.addListener(document, 'click', function(){}, false);
+         */
         'addListener': function addListener(el, evt, fn, bubbles) {
             el[addHandler](evtUtility(evt), fn, bubbles || false);
         },
+        /**
+         * Attach a handler to an event for the {HTMLElement} that executes only
+         * once.
+         *
+         * @memberof ch.Event
+         * @type {Function}
+         * @param {HTMLElement} el An HTMLElement to add listener to
+         * @param {String} evt Event name
+         * @param {Function} fn Event handler function
+         * @param {Boolean} bubbles Whether or not to be propagated to outer elements.
+         * @example
+         * ch.Event.addListenerOne(document, 'click', function(){}, false);
+         */
         'addListenerOne': function addListener(el, evt, fn, bubbles) {
 
             function oneRemove() {
@@ -115,14 +150,35 @@ ch.Event = (function () {
 
             // must remove the event after executes one time
             el[addHandler](evtUtility(evt), fn, bubbles || false);
-            // TODO: Review this method, wrong looks like has wrong behavior
+            // TODO: Review this method, looks like has wrong behavior when listener should be removed
             el[addHandler](evtUtility(evt), function () {
                 oneRemove()
             }, bubbles || false);
         },
+        /**
+         * Crossbrowser implementation of {HTMLElement}.removeEventListener.
+         *
+         * @memberof ch.Event
+         * @type {Function}
+         * @param {HTMLElement} el An HTMLElement to remove listener from
+         * @param {String} evt Event name
+         * @param {Function} fn Event handler function to remove
+         * @example
+         * ch.Event.removeListener(document, 'click', fn);
+         */
         'removeListener': function removeListener(el, evt, fn) {
             el[removeHandler](evtUtility(evt), fn);
         },
+        /**
+         * Crossbrowser implementation of {HTMLElement}.removeEventListener.
+         *
+         * @memberof ch.Event
+         * @type {Function}
+         * @param {HTMLElement} el An HTMLElement to dispatch event to
+         * @param {String|Event} evt Event name or event object
+         * @example
+         * ch.Event.dispatchEvent(document, 'click');
+         */
         'dispatchEvent': function dispatchEvent(el, e) {
             var event = e;
 
@@ -132,6 +188,23 @@ ch.Event = (function () {
             }
             el[dispatch](event);
         },
+        /**
+         * Dispatches the custom event that is not the part of standard DOM Event
+         *
+         * @memberof ch.Event
+         * @type {Function}
+         * @param {HTMLElement} el An HTMLElement to dispatch event to
+         * @param {String} name Custom event name
+         * @param {Object} params Optional event params that should be passed to an event
+         * @example
+         * ch.Event.dispatchCustomEvent(document, ch.onlayoutchange, {
+         *   bubbles: true,
+         *   cancelable: false,
+         *   detail: {
+         *     x: 123
+         *   }
+         * });
+         */
         'dispatchCustomEvent': function dispatchCustomEvent(el, name, params) {
             if (!_custom[name]) {
                 var data = ch.util.extend({
@@ -153,132 +226,3 @@ ch.Event = (function () {
         }
     }
 }());
-
-/**
- * Normalizes touch/touch+click events into a 'pointertap' event that is not
- * part of standard.
- * Uses pointerEvents polyfill or native PointerEvents when supported.
- *
- * @example
- * // Use pointertap as fastclick on touch enabled devices
- * document.querySelector('.btn').addEventListener(ch.pointertap, function(e) {
- *   console.log('tap');
- * });
- */
-(function () {
-    'use strict';
-
-    // IE8 has no support for custom Mouse Events, fallback to onclick
-    // Use an original click on UAs with no touch
-    if (!window.MouseEvent || !ch.support.touch) {
-        return;
-    }
-
-    var POINTER_TYPE_TOUCH = "touch";
-    var POINTER_TYPE_PEN = "pen";
-    var POINTER_TYPE_MOUSE = "mouse";
-
-    var isScrolling = false;
-    var scrollTimeout = false;
-    var sDistX = 0;
-    var sDistY = 0;
-    var activePointer;
-
-    window.addEventListener('scroll', function () {
-        if (!isScrolling) {
-            sDistX = window.pageXOffset;
-            sDistY = window.pageYOffset;
-        }
-        isScrolling = true;
-        clearTimeout(scrollTimeout);
-        scrollTimeout = setTimeout(function () {
-            isScrolling = false;
-            sDistX = 0;
-            sDistY = 0;
-        }, 100);
-    });
-
-    window.addEventListener('pointerdown', pointerDown);
-    window.addEventListener('pointerup', pointerUp);
-    window.addEventListener('pointerleave', pointerLeave);
-
-    window.addEventListener('pointermove', function (e) {});
-
-    /**
-     * Handles the 'pointerdown' event from pointerEvents polyfill or native PointerEvents when supported.
-     *
-     * @private
-     * @param {MouseEvent|PointerEvent} e Event.
-     */
-    function pointerDown(e) {
-        // don't register an activePointer if more than one touch is active.
-        var singleFinger = e.pointerType === POINTER_TYPE_MOUSE ||
-            e.pointerType === POINTER_TYPE_PEN ||
-            (e.pointerType === POINTER_TYPE_TOUCH && e.isPrimary);
-
-        if (!isScrolling && singleFinger) {
-            activePointer = {
-                id: e.pointerId,
-                clientX: e.clientX,
-                clientY: e.clientY,
-                x: (e.x || e.pageX),
-                y: (e.y || e.pageY),
-                type: e.pointerType
-            }
-        }
-    }
-
-    /**
-     * Handles the 'pointerleave' event from pointerEvents polyfill or native PointerEvents when supported.
-     *
-     * @private
-     * @param {MouseEvent|PointerEvent} e Event.
-     */
-    function pointerLeave(e) {
-        activePointer = null;
-    }
-
-    /**
-     * Handles the 'pointerup' event from pointerEvents polyfill or native PointerEvents when supported.
-     *
-     * @private
-     * @param {MouseEvent|PointerEvent} e Event.
-     */
-    function pointerUp(e) {
-        // Does our event is the same as the activePointer set by pointerdown?
-        if (activePointer && activePointer.id === e.pointerId) {
-            // Have we moved too much?
-            if (Math.abs(activePointer.x - (e.x || e.pageX)) < 5 &&
-                Math.abs(activePointer.y - (e.y || e.pageY)) < 5) {
-                // Have we scrolled too much?
-                if (!isScrolling ||
-                    (Math.abs(sDistX - window.pageXOffset) < 5 &&
-                    Math.abs(sDistY - window.pageYOffset) < 5)) {
-                    makePointertapEvent(e);
-                }
-            }
-        }
-        activePointer = null;
-    }
-
-    /**
-     * Creates the pointertap event that is not part of standard.
-     *
-     * @private
-     * @param {MouseEvent|PointerEvent} sourceEvent An event to use as a base for pointertap.
-     */
-    function makePointertapEvent(sourceEvent) {
-        var evt = document.createEvent('MouseEvents');
-        var newTarget = document.elementFromPoint(sourceEvent.clientX, sourceEvent.clientY);
-
-        // TODO: Replace 'initMouseEvent' with 'new MouseEvent'
-        evt.initMouseEvent('pointertap', true, true, window, 1, sourceEvent.screenX, sourceEvent.screenY,
-            sourceEvent.clientX, sourceEvent.clientY, sourceEvent.ctrlKey, sourceEvent.altKey,
-            sourceEvent.shiftKey, sourceEvent.metaKey, sourceEvent.button, newTarget);
-
-        evt.maskedEvent = sourceEvent;
-        newTarget.dispatchEvent(evt);
-
-        return evt;
-    }
-})();
