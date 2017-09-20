8a1da2dd4c19b299857d8a988fd740506705835f
Constanza Uanini <constanza.uanini@mercadolibre.com>
2017-09-20 12:09:26 -0300

    Validate if keyCode exists

--- src/ui/scripts/shortcuts.js
+++ src/ui/scripts/shortcuts.js
@@ -152,27 +152,24 @@
             }
         },
         shortcutsEmitter = function (event) {
-            if (event.keyCode) {
-                var keyCode = event.keyCode.toString(),
+            var keyCode = event.keyCode && event.keyCode.toString(),
                 shortcut = codeMap[keyCode],
                 callbacks,
                 callbacksLenght,
                 i = 0;
 
-                if (shortcut !== undefined && shortcuts._active !== null) {
-                    callbacks = shortcuts._collection[shortcuts._active][shortcut];
+            if (shortcut !== undefined && shortcuts._active !== null) {
+                callbacks = shortcuts._collection[shortcuts._active][shortcut];
 
-                    event.shortcut = shortcut;
+                event.shortcut = shortcut;
 
 
-                    if (callbacks !== undefined) {
+                if (callbacks !== undefined) {
 
-                        callbacksLenght = callbacks.length;
+                    callbacksLenght = callbacks.length;
 
-                        for (i = 0; i < callbacksLenght; i += 1) {
-                            callbacks[i](event);
-                        }
-
+                    for (i = 0; i < callbacksLenght; i += 1) {
+                        callbacks[i](event);
                     }
 
                 }
